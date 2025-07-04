9f68d14c3deb71b571d0fc9e7c71d95214c482da
fox <textfox10000@gmail.com>
2025-07-04 23:22:29 +0300

    Add project files.

---
+++ Assignment 01/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(value: "Hello, World!");
+        }
+    }
+}
